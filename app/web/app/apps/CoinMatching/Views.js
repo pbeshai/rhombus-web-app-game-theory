116b75f3df15bd5d8ebc663f299a68420c22312b
Peter Beshai <peter.beshai@gmail.com>
2013-10-08 11:21:42 -0700

    Refactored views for coin matching

--- app/web/app/apps/CoinMatching/Views.js
+++ app/web/app/apps/CoinMatching/Views.js
@@ -24,107 +24,51 @@ function (App, Common, CoinMatching) {
 
 	CoinMatchingViews.Play = {};
 
-	CoinMatchingViews.Play.Participant = Common.Views.ParticipantHiddenPlay.extend({
-		bottomText: function (model) {
-			if (model.get("score") != null) {
-				return "Prev. " + model.get("score");
-			}
-		}
-	});
+	CoinMatchingViews.Play.Participant = Common.Views.ParticipantHiddenPlay;
 
-	function total(collection, attribute) {
-		return collection.reduce(function (memo, p) {
-			return memo + (p.get(attribute) || 0);
-		}, 0);
-	}
-
-	CoinMatchingViews.Play.Layout = App.registerView("coin-matching::play", Common.Mixins.rounds(Common.Views.GroupLayout.extend({
+	CoinMatchingViews.Play.Layout = App.registerView("coin-matching::play", Common.Mixins.mixin(["rounds", "phaseTotals"], Common.Views.GroupLayout.extend({
 		header: "Play",
 		ParticipantView: CoinMatchingViews.Play.Participant,
 		InstructionsModel: CoinMatching.Instructions,
-		group1HeaderRight: function () { return total(this.model.get("group1"), "phaseTotal"); },
-		group2HeaderRight: function () { return total(this.model.get("group2"), "phaseTotal"); }
 	})));
 
 	CoinMatchingViews.Results = {};
 
-	CoinMatchingViews.Results.Score = Common.Mixins.bucketParticipant(Common.Views.ParticipantDisplay.extend({
-		locked: true,
-
-		overlay: function (model) {
-			return "no-animate";
-		},
-
-		mainText: function (model) {
-			var choice = CoinMatching.Util.labelChoice(model.get("choice")),
-					partnerChoice = CoinMatching.Util.labelChoice(model.get("partner").get("choice"));
-
-			var outcome;
-			if (model.get("role") === "row") {
-				outcome = choice+partnerChoice;
-			} else {
-				outcome = partnerChoice+choice;
-			}
-			return outcome + " "+model.get("score");
-		},
-		bottomText: function (model) {
-			if (model.get("phaseTotal") != null) {
-				return "Total " + model.get("phaseTotal");
-			}
-		}
+	CoinMatchingViews.Results.Score = Common.Mixins.bucketParticipant(Common.Views.ParticipantScoreChoiceDisplay.extend({
+		labelChoice: CoinMatching.Util.labelChoice
 	}));
 
-	CoinMatchingViews.Results.Layout = App.registerView("coin-matching::results", Common.Mixins.rounds(Common.Views.GroupLayout.extend({
+	CoinMatchingViews.Results.Layout = App.registerView("coin-matching::results", Common.Mixins.mixin(["rounds", "phaseTotals"], Common.Views.GroupLayout.extend({
 		header: "Results",
 		className: "coin-matching-results",
 		ParticipantView: CoinMatchingViews.Results.Score,
-		group1HeaderRight: function () { return total(this.model.get("group1"), "phaseTotal"); },
-		group2HeaderRight: function () { return total(this.model.get("group2"), "phaseTotal"); },
 	})));
 
 
 	CoinMatchingViews.PhaseResults = {};
 
-	CoinMatchingViews.PhaseResults.Score = Common.Mixins.bucketParticipant(Common.Views.ParticipantDisplay.extend({
-		locked: true,
-		overlay: function (model) {
-			return "no-animate";
-		},
-
-		mainText: function (model) {
-			return model.get("phaseTotal");
-		},
+	CoinMatchingViews.PhaseResults.Score = Common.Mixins.bucketParticipant(Common.Views.ParticipantScoreDisplay.extend({
+		scoreAttribute: "phaseTotal"
 	}));
 
-	CoinMatchingViews.PhaseResults.Layout = App.registerView("coin-matching::phase-results", Common.Views.GroupLayout.extend({
+	CoinMatchingViews.PhaseResults.Layout = App.registerView("coin-matching::phase-results", Common.Mixins.phaseTotals(Common.Views.GroupLayout.extend({
 		header: "Results for Phase ",
 		className: "coin-matching-results",
 		ParticipantView: CoinMatchingViews.PhaseResults.Score,
-		group1HeaderRight: function () { return total(this.model.get("group1"), "phaseTotal"); },
-		group2HeaderRight: function () { return total(this.model.get("group2"), "phaseTotal"); }
-	}));
+	})));
 
 
 	CoinMatchingViews.TotalResults = {};
 
-	CoinMatchingViews.TotalResults.Score = Common.Mixins.bucketParticipant(Common.Views.ParticipantDisplay.extend({
-		locked: true,
-		overlay: function (model) {
-			return "no-animate";
-		},
-
-		mainText: function (model) {
-			return model.get("total");
-		},
+	CoinMatchingViews.TotalResults.Score = Common.Mixins.bucketParticipant(Common.Views.ParticipantScoreDisplay.extend({
+		scoreAttribute: "total"
 	}));
 
-	CoinMatchingViews.TotalResults.Layout = App.registerView("coin-matching::total-results", Common.Views.GroupLayout.extend({
+	CoinMatchingViews.TotalResults.Layout = App.registerView("coin-matching::total-results", Common.Mixins.totals(Common.Views.GroupLayout.extend({
 		header: "Total Results",
 		className: "coin-matching-results",
 		ParticipantView: CoinMatchingViews.TotalResults.Score,
-		group1HeaderRight: function () { return total(this.model.get("group1"), "total"); },
-		group2HeaderRight: function () { return total(this.model.get("group2"), "total"); }
-	}));
+	})));
 
 	return CoinMatchingViews;
 });
