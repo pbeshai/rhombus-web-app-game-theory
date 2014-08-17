4baaf818f7fc9b6f76812038631ea8e172f2e647
Peter Beshai <peter.beshai@gmail.com>
2014-08-16 22:31:19 -0700

    Initial code to move server APIs to be with apps; PD done

---
+++ app/web/app/apps/PrisonersDilemma/server/api.js
@@ -0,0 +1,44 @@
+module.exports = {
+	init: init
+};
+
+var fs = require('fs'),
+	_ = require('lodash'),
+	logger = require("../../../../../../log/logger"),
+	util = require("../../../../../../framework/server/api/util");
+
+function init(site, initConfig) {
+	site.post("/api/apps/pd/log", pdResults);
+}
+
+function pdResults(req, res) {
+	var now = new Date();
+	var results = req.body.results;
+	var config = req.body.config;
+	var version = req.body.version;
+
+	var stream = fs.createWriteStream("log/pd/results." + util.filenameFormat(now) + ".csv");
+
+	stream.once('open', function(fd) {
+		function output (str) {
+			logger.info(str);
+			stream.write(str + "\n");
+		}
+		output("Prisoner's Dilemma Results (v" + version + ")");
+		output(now.toString());
+		if (config.message) {
+			output(config.message);
+		}
+		if (config.scoringMatrix) {
+			output("CC," + config.scoringMatrix.CC + ",CD," + config.scoringMatrix.CD);
+			output("DC," + config.scoringMatrix.DC + ",DD," + config.scoringMatrix.DD);
+		}
+
+		output("Alias,Choice,Payoff,PartnerAlias,PartnerChoice,PartnerPayoff");
+		_.each(results, function (result) {
+			output(result.alias + "," + result.choice + "," + result.score + "," + result.partner.alias + "," + result.partner.choice + "," + result.partner.score);
+		});
+		stream.end();
+	});
+	res.send(200);
+}
